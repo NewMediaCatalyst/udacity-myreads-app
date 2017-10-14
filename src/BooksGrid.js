11c482e5bde7b8080c4fa2bef68b94cf267c5a62
NewMediaCatalyst <newcatalystweb@gmail.com>
2017-10-14 10:03:24 -0700

    feat: Added 'loading' vs 'no results' to BooksGrid

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -7,15 +7,29 @@ import Book from './Book';
 
 class BooksGrid extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true
+        }
+    }
+
     static propTypes = {
         books: PropTypes.array.isRequired,
         page: PropTypes.string.isRequired,
         updateShelves: PropTypes.func
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.books !== undefined && nextProps.books.length >= 0) {
+            this.setState({ loading: false });
+        }
+    }
+
     render() {
         const {books, page, updateShelves} = this.props;
-        let link = null;
+        let link = null, text = "Loading books...",
+            {loading} = this.state;
 
         if (books !== null && books !== undefined && books.length > 0) {
             return <ol className="books-grid">
@@ -31,11 +45,16 @@ class BooksGrid extends Component {
                 )}
             </ol>
         }
-        if (page === "home" || page === undefined) {
-            link = <Link to="/search">Add a book &raquo;</Link>
+
+        if (!loading) {
+            if (page === "home" || page === undefined) {
+                link = <Link to="/search">Add a book &raquo;</Link>
+            }
+            text = "No books, currently! ";
         }
+
         return <div className="no-results">
-            {"No books, currently! "}
+            {text}
             {link}
         </div>
     }
